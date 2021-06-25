c3352cd0d9a6fd7d8393ace2d733a359657a394c
AlyxMoon <allisterkmoon@gmail.com>
2021-06-25 15:33:55 -0700

    add an indicator when a planet is selected

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 // import GalacticAgeCalculator from './lib/GalaticAgeCalculator'
 import 'purecss/build/base-min.css'
 import '@/styles/main.scss'
+
+const planetRadios = document.querySelectorAll('input[name="planet"]')
+
+for (const radio of planetRadios) {
+  radio.addEventListener('change', (event) => {
+    console.log('updated selected planet')
+  })
+}
