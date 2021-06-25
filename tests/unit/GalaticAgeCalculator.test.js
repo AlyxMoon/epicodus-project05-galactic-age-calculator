856663428db5451145e2cdfd9e752430e8fc4391
AlyxMoon <allisterkmoon@gmail.com>
2021-06-25 09:47:22 -0700

    add second test: check for ages to be initialized

--- tests/unit/GalaticAgeCalculator.test.js
+++ tests/unit/GalaticAgeCalculator.test.js
@@ -5,4 +5,19 @@ describe('Class GalaticAgeCalculator', () => {
     const calculator = new GalacticAgeCalculator()
     expect(calculator).toBeTruthy()
   })
+
+  it('should start with all the ages set to 0', () => {
+    const calculator = new GalacticAgeCalculator()
+    expect(calculator.ages).toEqual({
+      mercury: 0,
+      venus: 0,
+      earth: 0,
+      mars: 0,
+      jupiter: 0,
+      saturn: 0,
+      uranus: 0,
+      neptune: 0,
+      pluto: 0,
+    })
+  })
 })
