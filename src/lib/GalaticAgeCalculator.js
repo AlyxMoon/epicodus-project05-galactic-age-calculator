4180ee0369b11fd32355769e7ffeefdebc2bb416
AlyxMoon <allisterkmoon@gmail.com>
2021-06-25 09:48:53 -0700

    pass test: check if ages are initialized correctly

--- src/lib/GalaticAgeCalculator.js
+++ src/lib/GalaticAgeCalculator.js
@@ -1,6 +1,16 @@
 
 export default class GalacticAgeCalculator {
   constructor () {
-    this.something = 42
+    this.ages = {
+      mercury: 0,
+      venus: 0,
+      earth: 0,
+      mars: 0,
+      jupiter: 0,
+      saturn: 0,
+      uranus: 0,
+      neptune: 0,
+      pluto: 0,
+    }
   }
 }
