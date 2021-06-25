d28bde057e850aa2c568eb89651f52372dd747c6
AlyxMoon <allisterkmoon@gmail.com>
2021-06-25 10:37:44 -0700

    add additional test: add extra expect to roundToDecimal() test

--- tests/unit/roundToDecimal.test.js
+++ tests/unit/roundToDecimal.test.js
@@ -3,5 +3,6 @@ import roundToDecimal from '@/lib/roundToDecimal'
 describe('roundToDecimal()', () => {
   it('should correctly round to the 0th decimal (default option)', () => {
     expect(roundToDecimal(45.3)).toEqual(45)
+    expect(roundToDecimal(45.5)).toEqual(46)
   })
 })
