b292d336eaca09b50d82dbbac13e8c09cef0516c
AlyxMoon <allisterkmoon@gmail.com>
2021-06-25 11:37:25 -0700

    pass tests: pass the agesLeft prop check on initalization

--- src/lib/GalaticAgeCalculator.js
+++ src/lib/GalaticAgeCalculator.js
@@ -16,6 +16,18 @@ export default class GalacticAgeCalculator {
       pluto: 0,
     }
 
+    this.agesLeft = {
+      mercury: 0,
+      venus: 0,
+      earth: 0,
+      mars: 0,
+      jupiter: 0,
+      saturn: 0,
+      uranus: 0,
+      neptune: 0,
+      pluto: 0,
+    }
+
     this.lifeExpectancy = lifeExpectancy
 
     this.multOffsetFromEarth = {
