649cf155c9fd7be6f3ba2ca6b40ef48684f60d85
AlyxMoon <allisterkmoon@gmail.com>
2021-06-25 16:28:10 -0700

    connect inputs to calculator

--- src/main.js
+++ src/main.js
@@ -1,23 +1,76 @@
-// import GalacticAgeCalculator from './lib/GalaticAgeCalculator'
+import GalacticAgeCalculator from './lib/GalaticAgeCalculator'
 import 'purecss/build/pure-min.css'
 import '@/styles/main.scss'
 
-const planetRadios = document.querySelectorAll('input[name="planet"]')
+const addSidebarEventListeners = () => {
+  document
+    .querySelector('.sidebar button[data-action="collapse-sidebar"]')
+    .addEventListener('click', () => {
+      document.querySelector('.sidebar').classList.add('collapsed')
 
-for (const radio of planetRadios) {
-  radio.addEventListener('change', (event) => {
-    console.log('updated selected planet')
+      const radio = document.querySelector('input[name="planet"]:checked')
+      if (radio) radio.checked = false
+    })
+
+  document
+    .querySelector('.sidebar button[data-action="expand-sidebar"]')
+    .addEventListener('click', () => {
+      document.querySelector('.sidebar').classList.remove('collapsed')
+    })
+}
+
+const addPlanetEventListeners = (calculator) => {
+  const planetRadios = document.querySelectorAll('input[name="planet"]')
+  const planetLabel = document.querySelector('#active-planet-label')
+
+  for (const radio of planetRadios) {
+    radio.addEventListener('change', (event) => {
+      document.querySelector('.sidebar').classList.remove('collapsed')
+
+      planetLabel.innerText = event.target.value
+    })
+  }
+}
+
+const addAgeInputListeners = (calculator) => {
+  document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault()
+  })
+
+  const inputAge = document.querySelector('#input-age')
+  const inputExpectancy = document.querySelector('#input-expectancy')
+
+  inputAge.value = calculator.ages.earth
+  inputExpectancy.value = calculator.lifeExpectancy
+
+  inputAge.addEventListener('change', (event) => {
+    const num = parseInt(event.target.value)
+
+    if (Number.isNaN(num)) {
+      event.target.value = 0
+    } else {
+      calculator.setAge(num)
+    }
+  })
+
+  inputExpectancy.addEventListener('change', (event) => {
+    const num = parseInt(event.target.value)
+
+    if (Number.isNaN(num)) {
+      event.target.value = 0
+    } else {
+      calculator.lifeExpectancy = num
+    }
   })
 }
 
-document
-  .querySelector('.sidebar button[data-action="collapse-sidebar"]')
-  .addEventListener('click', () => {
-    document.querySelector('.sidebar').classList.add('collapsed')
-  })
+const main = () => {
+  const calculator = new GalacticAgeCalculator()
+  calculator.setAge(1)
 
-document
-  .querySelector('.sidebar button[data-action="expand-sidebar"]')
-  .addEventListener('click', () => {
-    document.querySelector('.sidebar').classList.remove('collapsed')
-  })
+  addSidebarEventListeners()
+  addPlanetEventListeners(calculator)
+  addAgeInputListeners(calculator)
+}
+
+main()
