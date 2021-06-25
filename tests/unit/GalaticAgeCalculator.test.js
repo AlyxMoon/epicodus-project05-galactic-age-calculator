e290a46cf348168834e6ccfc226fac681b2d771b
AlyxMoon <allisterkmoon@gmail.com>
2021-06-25 10:55:05 -0700

    add test: setting age with mercury as a base

--- tests/unit/GalaticAgeCalculator.test.js
+++ tests/unit/GalaticAgeCalculator.test.js
@@ -35,4 +35,19 @@ describe('Class GalaticAgeCalculator', () => {
     expect(calculator.ages.neptune).toBeCloseTo(0.61)
     expect(calculator.ages.pluto).toBeCloseTo(0.40)
   })
+
+  it('should set all ages correctly when setting mercury age (rounded 2 decimal places with some allowed variance)', () => {
+    const calculator = new GalacticAgeCalculator()
+    calculator.setAge(100, 'mercury')
+
+    expect(calculator.ages.mercury).toBeCloseTo(100.00)
+    expect(calculator.ages.venus).toBeCloseTo(39.15)
+    expect(calculator.ages.earth).toBeCloseTo(24.08)
+    expect(calculator.ages.mars).toBeCloseTo(12.80)
+    expect(calculator.ages.jupiter).toBeCloseTo(2.03)
+    expect(calculator.ages.saturn).toBeCloseTo(0.82)
+    expect(calculator.ages.uranus).toBeCloseTo(0.29)
+    expect(calculator.ages.neptune).toBeCloseTo(0.15)
+    expect(calculator.ages.pluto).toBeCloseTo(0.10)
+  })
 })
